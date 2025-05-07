9d5f54027fa6be7fd8f93b16b08ef196d0b99253
Sakil <sakil@ishivax.com>
2025-05-07 12:19:58 +0530

    new here

--- src/CRMProvider.php
+++ src/CRMProvider.php
@@ -9,21 +9,20 @@ class CRMProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        $key = env('PROJECT_LICENSE_KEY'); 
         $validLicenseKey = 'abc1234';
 
         if ($key !== $validLicenseKey) {
-            Log::critical('Invalid license key or license missing.');
+            \Log::critical('Invalid license key or license missing.');
 
             if (php_sapi_name() === 'cli') {
-                echo "⚠️  Invalid or missing license key. Please contact the developer.\\n";
+                echo "⚠️  Invalid or missing license key. Please contact the developer.\n";
             } else {
                 echo "<h1>Maintenance Mode</h1><p>This application is not licensed to run. Please contact support.</p>";
             }
 
             exit;
         }
-
     }
 
     public function register()
